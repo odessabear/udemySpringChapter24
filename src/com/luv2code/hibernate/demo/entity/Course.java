97ebd0e92587070c38dcede113b0f8257e98ada3
odessabear <bearodessa@gmail.com>
2019-11-22 19:03:46 +0200

    first commit

---
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -0,0 +1,20 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	
+	//define our fields
+	
+	//define constructors
+	
+	//define getter setters
+	
+	//define toString method
+	
+	//annotate fields
+
+}
